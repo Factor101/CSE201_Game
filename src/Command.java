c9f064908f8031457cb46515fd9bbd0db74efab2
NathanAnthony <113609129+NathanAnthony@users.noreply.github.com>
2024-11-01 13:25:43 -0400

    Update Command.java
    Updated Command

--- src/Command.java
+++ src/Command.java
@@ -1,5 +1,60 @@
-package src;
+import java.awt.Point;
 
-public class Command
-{
+public class Command {
+    /**
+     * Creates new command
+     */
+    public Command() {
+
+    }
+
+    /**
+     * Command to go with no direction
+     * 
+     * @return message telling player to specify a direction
+     */
+    public static String go() {
+        return "Go where?";
+    }
+
+    /**
+     * Command for going north
+     * 
+     * @return a point value to be added to current room position to determine
+     *         if the player can move to an new room
+     */
+    public static Point goNorth() {
+        return new Point(0, 1);
+    }
+    
+    /**
+     * Command for going east
+     * 
+     * @return a point value to be added to current room position to determine
+     *         if the player can move to an new room
+     */
+    public static Point goEast() {
+        return new Point(1, 0);
+    }
+    
+    /**
+     * Command for going south
+     * 
+     * @return a point value to be added to current room position to determine
+     *         if the player can move to an new room
+     */
+    public static Point goSouth() {
+        return new Point(0, -1);
+    }
+    
+    /**
+     * Command for going west
+     * 
+     * @return a point value to be added to current room position to determine
+     *         if the player can move to an new room
+     */
+    public static Point goWest() {
+        return new Point(-1, 0);
+    }
+
 }
