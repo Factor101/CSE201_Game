0f9e845c3345c468268e33c6564afa5254510b3d
mattheffo <mattheffo14@gmail.com>
2024-11-01 13:15:21 -0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,38 @@
 package src;
 
-public class Player
-{
+/**
+ * Class to represent the player, containing their
+ * info and possible actions.
+ * @version 1.0
+ */
+public class Player {
+	// The inventory of the player.
+	private Item[] inventory;
+	
+	// The current room the player is in.
+	private Room currentRoom;
+	
+	// The limit of items a player can hold.
+	final private int inventoryLimit = 5;
+	
+	/**
+	 * Constructor method.
+	 */
+	Player() {
+		this.inventory = new Item[inventoryLimit];
+	}
+	
+	/**
+	 * Method to determine if the player has a given item.
+	 * @param item - The item being tested for.
+	 * @return True if the player has the item, false otherwise.
+	 */
+	public boolean hasItem(Item item) {
+		for (int i = 0; i < inventory.length; i++) {
+			if (inventory[i] == item) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
