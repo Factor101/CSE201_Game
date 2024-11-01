57b13a584b29014174a3f6fc89e2cd9201272c62
NathanAnthony <anthonnp@miamioh.edu>
2024-11-01 14:07:31 -0400

    Commit Room class

--- src/Room.java
+++ src/Room.java
@@ -1,5 +1,33 @@
 package src;
 
-public class Room
-{
+import java.awt.Point;
+
+public class Room {
+    private Item[] itemsInRoom;
+    private String roomDescription;
+    private Point roomPosition;
+    private Command[] roomSpecificCommands;
+    
+    public Room(Item[] i, String desc, Point p, Command[] cm) {
+        this.itemsInRoom = i;
+        this.roomDescription = desc;
+        this.roomPosition = p;
+        this.roomSpecificCommands = cm;
+    }
+    
+    public Item[] getItems() {
+        return itemsInRoom;
+    }
+    
+    public String getDescription() {
+        return roomDescription;
+    }
+    
+    public Point getRoomPosition() {
+        return roomPosition;
+    }
+    
+    public Command[] getRoomCommands() {
+        return roomSpecificCommands;
+    }
 }
