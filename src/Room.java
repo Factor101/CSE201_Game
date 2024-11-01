d15f321fc9372be2693b914a3e7f7e24a5f1ce39
NathanAnthony <113609129+NathanAnthony@users.noreply.github.com>
2024-11-01 13:19:59 -0400

    Update Room.java
    Updated Room

--- src/Room.java
+++ src/Room.java
@@ -1,5 +1,54 @@
-package src;
+import java.awt.Point;
 
-public class Room
-{
+public class Room {
+    private Item[] itemsInRoom;
+    private String roomDescription;
+    private Point roomPosition;
+    private Command[] roomSpecificCommands;
+    
+    /**
+     * Creates a new room
+     * @param i list of items in the room
+     * @param desc description of the room
+     * @param p room's position represented as a Point
+     * @param cm list of commands unique to this room
+     */
+    public Room(Item[] i, String desc, Point p, Command[] cm) {
+        this.itemsInRoom = i;
+        this.roomDescription = desc;
+        this.roomPosition = p;
+        this.roomSpecificCommands = cm;
+    }
+    
+    /**
+     * Returns the items in this room
+     * @return the items in this room
+     */
+    public Item[] getItems() {
+        return itemsInRoom;
+    }
+    
+    /**
+     * Returns the room's description
+     * @return the room's description
+     */
+    public String getDescription() {
+        return roomDescription;
+    }
+    
+    /**
+     * Returns the room's position
+     * @return the room's position
+     */
+    public Point getRoomPosition() {
+        return roomPosition;
+    }
+    
+    /**
+     * Returns the room's unique commands
+     * @return the room's unique commands
+     */
+    public Command[] getRoomCommands() {
+        return roomSpecificCommands;
+    }
 }
